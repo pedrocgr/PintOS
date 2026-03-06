c296fc384109d5ca9cbc7c746e775eebda3629eb
Maria Eduarda <mevm@cin.ufpe.br>
2026-03-06 16:23:21 -0300

    add alarm-clock logic

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,6 +88,7 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    int64_t wakeup_tick;                 /* Tick to wake up sleeping thread. */
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
@@ -125,6 +126,10 @@ const char *thread_name (void);
 
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
+
+bool thread_wake_cmp (const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
+void thread_wake (int64_t ticks);
+void thread_sleep (int64_t ticks);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
